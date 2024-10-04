392947ed3c8c5e8d19c7e9a01fd22c29c5ed9456
pcsishun <pcsishun@gmail.com>
2024-10-04 12:06:53 +0700

    update

--- test_image_opencv.py
+++ test_image_opencv.py
@@ -13,47 +13,81 @@ def calculate_center(contours):
             array_center_y.append(cy)
     return (array_center_x, array_center_y)
 
+
+def find_bounding_box_frame(gray_frame):
+    ### config noise frame ###
+    blurred = cv2.GaussianBlur(gray_frame, (85,85), 0)
+    _, thresh = cv2.threshold(blurred, 160, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    kernel = np.ones((300, 300), np.uint8)
+    thresh = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
+    ### end config noise frame ###
+    contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    return contours, thresh
+
+def find_bounding_box_light(gray_frame):
+    ### config noise target ###
+    blurred = cv2.GaussianBlur(gray_frame, (55, 55), 0) ## Gaussian blur to reduce noise
+    _, thresh = cv2.threshold(blurred, 180, 255, cv2.THRESH_BINARY)
+    kernel = np.ones((3, 3), np.uint8)
+    thresh = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
+    ### end config noise target ###
+    contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    return contours, thresh
+
 def main():
-    image = cv2.imread('/Users/pcsishun/project_solar_thermal/gui_solar_control/test.png')
+    x_center_light = 0
+    y_center_light = 0
+    x_center_frame = 0
+    y_center_frame = 0
+
+    image = cv2.imread('/Users/pcsishun/project_solar_thermal/gui_solar_control/test9.png')
     frame = cv2.flip(image, 0)
     frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     
-    blurred = cv2.GaussianBlur(frame_gray, (25, 25), 0) ## Gaussian blur to reduce noise
-    _, thresh = cv2.threshold(blurred, 201, 255, cv2.THRESH_BINARY  + cv2.THRESH_OTSU)
+    ### convert frame to black and white
+    contours_frame, debug_thresh_frame = find_bounding_box_frame(frame_gray)
+    contours_light, debug_thresh_light = find_bounding_box_light(frame_gray)
 
-    # Erosion to remove small white noise, dilation to close gaps
-    kernel = np.ones((3, 3), np.uint8)
-    thresh = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
+    ### find the center of frame ###
+    center_x_light, center_y_light = calculate_center(contours_light)
+    center_x_frame, center_y_frame = calculate_center(contours_frame)
 
-    contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    center_x, center_y = calculate_center(contours)
+    ### draw main center of target ###
+    center_color_target = 0
+    for idx,el in  enumerate(center_x_light):
+        cv2.circle(frame, (center_x_light[idx], center_y_light[idx]), 14,(255,0,center_color_target),-1 )
+        cv2.putText(frame, "center_x_light: "+ str(center_x_light[idx]) + " " + "center_y_light: "+str(center_y_light[idx]) ,(center_x_light[idx] - 200, center_y_light[idx] + 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,center_color_target), 3)
+        center_color_target += 100
+        x_center_light = center_x_light[idx]
+        y_center_light = center_y_light[idx]
 
-    cnt = contours[0]
-    area = cv2.contourArea(cnt)
-    perimeter = cv2.arcLength(cnt,True)
-    rect = cv2.minAreaRect(cnt)
-    box = cv2.boxPoints(rect)
-    box = np.intp(box)
+    ### draw main center of frame ###
+    center_color_frame = 0
+    for idx,el in  enumerate(center_x_frame):
+        cv2.circle(frame, (center_x_frame[idx], center_y_frame[idx]), 14,(0,255,center_color_frame),-1 )
+        cv2.putText(frame, "center_x_frame: "+ str(center_x_frame[idx]) + " " + "center_y_frame: "+str(center_y_frame[idx]) ,(center_x_frame[idx], center_y_frame[idx] + 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,center_color_frame), 3)
+        center_color_frame += 100
+        x_center_frame = center_x_frame[idx]
+        y_center_frame = center_y_frame[idx]
 
-    x,y,w,h = cv2.boundingRect(cnt)
-
-    center_color = 0
-    for idx,el in  enumerate(center_x):
-        ### draw main center of target ###
-        if idx != 0:
-
-            center_color += 50
-            cv2.circle(frame, (center_x[idx], center_y[idx]), 14,(0,255,center_color),-1 )
-            cv2.putText(frame, "center_x: "+ str(center_x[idx]) + " " + "center_y: "+str(center_y[idx]) ,(center_x[idx], center_y[idx] + 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (center_color,0,0), 3)
-
-    center_color = 0
-    for cnt in contours:
-        center_color += 50
+    ### draw bounding box of target ###
+    area_color_target = 0
+    for cnt in contours_light:
         x,y,w,h = cv2.boundingRect(cnt)
-        cv2.putText(frame, "X:"+str(x) + " " + "Y:"+str(y) +" "+ "W:"+str(w) +" "+ "H:"+str(h),(x,h), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,center_color), 4)
+        cv2.putText(frame, "X:"+str(x) + " " + "Y:"+str(y) +" "+ "W:"+str(w) +" "+ "H:"+str(h),(x,y+50), cv2.FONT_HERSHEY_SIMPLEX, 1,  (255,0,area_color_target), 4)
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),10)
+        area_color_target += 100
 
-    cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),2)
-    cv2.drawContours(frame,[box],0,(0,0,255),10)
+    ### draw bounding box of frame ###
+    area_color_bounding_box_frame = 0
+    for cnt in contours_frame:
+        x,y,w,h = cv2.boundingRect(cnt)
+        cv2.putText(frame, "X:"+str(x) + " " + "Y:"+str(y) +" "+ "W:"+str(w) +" "+ "H:"+str(h),(x,y+50), cv2.FONT_HERSHEY_SIMPLEX, 1,  (0,255,area_color_bounding_box_frame), 4)
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,area_color_bounding_box_frame),10)
+        area_color_bounding_box_frame += 100
+
+    ### draw error center ###
+    cv2.putText(frame, "Error_x: " + str(x_center_frame - x_center_light)+"px" + " " + "Error_y: " + str(y_center_frame - y_center_light)+"px" ,(x,h+50), cv2.FONT_HERSHEY_SIMPLEX, 1,  (0,255,255), 4)
 
     ### warp all draw in same image frame ### 
     frame_with_contours = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
