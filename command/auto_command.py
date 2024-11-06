ce5bfb8f61ec361ee24aa79ad5e86fadfd3f3754
pcsishun <dekearthsa@hotmail.com>
2024-11-06 10:43:02 +0700

    update: adding btn low_v

--- command/auto_command.py
+++ command/auto_command.py
@@ -317,13 +317,3 @@ class ControllerAuto(BoxLayout):
             self.__off_loop_auto_calculate_diff()
             self.show_popup(f"Error saving file:\n{str(e)}")    
 
-    def haddle_reset_default_threshold_low_v(self):
-        with open('./data/setting/setting.json', 'r') as file:
-            setting_data = json.load(file)
-        
-        setting_data['hsv_threshold']['low_v'] = 180 ## default low_v
-
-        with open("./data/setting/setting.json", "w") as file:
-            json.dump(setting_data, file, indent=4)
-        
-        self.ids.slider_hsv_low_v.value = setting_data['hsv_threshold']['low_v']
